de5e5ad9fbba3a483fbc4f95d640217b4b28339d
Kris Bunda <bunda3@yahoo.com>
2020-10-27 10:05:17 -0500

    init commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DynamicBreadCrumbs.Models;
+using DynamicBreadCrumbs.Filters;
+
+namespace DynamicBreadCrumbs.Controllers
+{
+	// if we extend Controller class with, say, "BaseController",
+	// and then extend all controllers from that, can just add 
+	// this attribute to "BaseController".
+	[BreadcrumbActionFilter]
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Models/BreadcrumbModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicBreadCrumbs.Models
+{
+	public class Breadcrumb
+	{
+		public string Text { get; set; }
+		public string Action { get; set; }
+		public string Controller { get; set; }
+		public bool Active { get; set; }
+	}
+}
